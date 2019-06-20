f76389243e2397587e079ae1ed1a8797725ff8c2
diegolana <diegolana@gmail.com>
2019-06-20 17:05:28 -0300

    Update motors.ino
    change motor direction (leo update)

--- websocket_host/motors.ino
+++ websocket_host/motors.ino
@@ -1,7 +1,9 @@
 
 // use first channel of 16 channels (started from zero)
 #define LEDC_CHANNEL_0     0
+#define LEDC_CHANNEL_1     1
 #define LEDC_CHANNEL_5     5
+#define LEDC_CHANNEL_6     6
 
 // use 13 bit precission for LEDC timer
 #define LEDC_TIMER_13_BIT  13
@@ -10,8 +12,12 @@
 #define LEDC_BASE_FREQ     5000
 
 // fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
-#define MOTOR_RIGTH_PIN 13
-#define MOTOR_LEFT_PIN 23
+#define MOTOR_RIGTH_PIN 27
+#define MOTOR_RIGTH_PIN_2 14
+#define MOTOR_RIGTH_ENABLE 12
+#define MOTOR_LEFT_PIN 25
+#define MOTOR_LEFT_PIN_2 26
+#define MOTOR_LEFT_ENABLE 33
 
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
@@ -27,19 +33,41 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
 }
 
 void setupMotors() {
-  // Setup timer and attach timer to a led pin
+  pinMode(MOTOR_RIGTH_PIN, OUTPUT);
+  pinMode(MOTOR_RIGTH_PIN_2, OUTPUT);
+  pinMode(MOTOR_RIGTH_ENABLE, OUTPUT);
+  
+  pinMode(MOTOR_LEFT_PIN_2, OUTPUT);
+  pinMode(MOTOR_RIGTH_ENABLE, OUTPUT);
+  pinMode(MOTOR_LEFT_ENABLE, OUTPUT);
+  
+  digitalWrite(MOTOR_RIGTH_ENABLE, HIGH);
+  digitalWrite(MOTOR_LEFT_ENABLE, HIGH);
+   
   ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcAttachPin(MOTOR_RIGTH_PIN, LEDC_CHANNEL_0);
+  
+  ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcAttachPin(MOTOR_RIGTH_PIN_2, LEDC_CHANNEL_1);
 
   ledcSetup(LEDC_CHANNEL_5, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcAttachPin(MOTOR_LEFT_PIN, LEDC_CHANNEL_5);
+
+  ledcSetup(LEDC_CHANNEL_6, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcAttachPin(MOTOR_LEFT_PIN_2, LEDC_CHANNEL_6);
+
 }
 
 void analogMotorRight(int value) {
   if (value < 1) {
-    value = value * -1;
+      value = value * -1;
+      ledcAnalogWrite(LEDC_CHANNEL_0, 0);
+      ledcAnalogWrite(LEDC_CHANNEL_1, value);
+  } else {
+      ledcAnalogWrite(LEDC_CHANNEL_0, value);
+      ledcAnalogWrite(LEDC_CHANNEL_1, 0);
   }
-  ledcAnalogWrite(LEDC_CHANNEL_0, value);
+
   Serial.print("Motor Right -> ");
   Serial.println(value);
   //delay(30);
@@ -48,8 +76,13 @@ void analogMotorRight(int value) {
 void analogMotorLeft(int value) {
   if (value < 1) {
     value = value * -1;
+    ledcAnalogWrite(LEDC_CHANNEL_5, 0);
+    ledcAnalogWrite(LEDC_CHANNEL_6, value);
+  } else {
+    ledcAnalogWrite(LEDC_CHANNEL_5, value);    
+    ledcAnalogWrite(LEDC_CHANNEL_6, 0);
   }
-  ledcAnalogWrite(LEDC_CHANNEL_5, value);
+
   Serial.print("Motor Left -> ");
   Serial.println(value);
   //delay(30);
